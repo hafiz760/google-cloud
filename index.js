83695fe852c546856dce23c594c283bbf1a91a26
Muhammad Bilal <bilal@dnaze.com>
2024-03-19 11:26:04 +0500

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // Create an Express application
 const app = express();
-const port = 3000;
+const port = 3000 || 4000;
 
 // Define a route
 app.get('/', (req, res) => {
